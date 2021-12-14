d6490f2fe3181fe9c1c5981ca0778b513848668e
conorfennell <fennell.conor@gmail.com>
2021-12-14 19:59:54 +0000

    add health controller test

---
+++ src/test/kotlin/com/idiomcentric/HealthControllerTest.kt
@@ -0,0 +1,27 @@
+package com.idiomcentric
+
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+@MicronautTest(packages = ["com.idiomcentric"])
+class HealthControllerTest {
+
+    @Inject
+    @field:Client("/")
+    lateinit var healthClient: HttpClient
+
+
+    @Test
+    fun testHealth() {
+        val actual: Health =
+            healthClient.toBlocking().retrieve(HttpRequest.GET<Health>("health"), Health::class.java)
+
+        Assertions.assertNotNull(actual, "health should not be null")
+    }
+}
+
