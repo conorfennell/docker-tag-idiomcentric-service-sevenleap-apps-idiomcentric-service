c76fb3988bc3f6e3782ff439e69e61a2d2239584
conorfennell <fennell.conor@gmail.com>
2021-12-18 22:35:27 +0000

    migrate build file to kts and use toolchains

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.6.0"
-    id("org.jetbrains.kotlin.kapt") version "1.6.0"
+    kotlin("jvm") version "1.6.0"
+    kotlin("kapt") version "1.6.0"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.6.0"
     id("com.github.johnrengelman.shadow") version "7.1.0"
     id("io.micronaut.application") version "3.0.1"
@@ -14,11 +14,12 @@ repositories {
 }
 
 micronaut {
+    version.set("3.2.1")
     runtime("netty")
     testRuntime("junit5")
     processing {
-        incremental(true)
-        annotations("com.idiomcentric.*")
+        incremental.set(true)
+        annotations.add("com.idiomcentric.*")
     }
 }
 
@@ -28,43 +29,32 @@ dependencies {
     implementation("io.swagger.core.v3:swagger-annotations")
     implementation("io.micronaut:micronaut-http-client")
     implementation("io.micronaut:micronaut-runtime")
+    implementation("io.micronaut:micronaut-validation")
     implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
     implementation("javax.annotation:javax.annotation-api")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC2")
     runtimeOnly("ch.qos.logback:logback-classic")
     implementation("ch.qos.logback.contrib:logback-json-classic:0.1.5")
     implementation("ch.qos.logback.contrib:logback-jackson:0.1.5")
-    implementation('io.github.microutils:kotlin-logging:2.1.16')
+    implementation("io.github.microutils:kotlin-logging:2.1.16")
     compileOnly("org.graalvm.nativeimage:svm")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("io.micronaut.test:micronaut-test-junit5:3.0.5")
-    testRuntimeOnly('org.junit.jupiter:junit-jupiter-engine:5.8.2')
-
-    implementation("io.micronaut:micronaut-validation")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC2")
-
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
 
 }
 
-
 application {
     mainClass.set("com.idiomcentric.Application")
 }
-java {
-    sourceCompatibility = JavaVersion.toVersion("11")
-}
 
-tasks {
-    compileKotlin {
-        kotlinOptions {
-            jvmTarget = "11"
-        }
-    }
-    compileTestKotlin {
-        kotlinOptions {
-            jvmTarget = "11"
-        }
+kotlin {
+    jvmToolchain {
+        (this as JavaToolchainSpec)
+        this.languageVersion.set(JavaLanguageVersion.of(11))
+        this.vendor.set(JvmVendorSpec.GRAAL_VM)
     }
 }
