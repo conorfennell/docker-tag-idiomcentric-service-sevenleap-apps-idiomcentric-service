57caccd744de35280c54611813d1836d0f446cb8
conorfennell <fennell.conor@gmail.com>
2021-12-22 15:04:25 +0000

    add cache to reddit call

--- src/main/kotlin/com/idiomcentric/service/RedditLowLevelClient.kt
+++ src/main/kotlin/com/idiomcentric/service/RedditLowLevelClient.kt
@@ -1,15 +1,17 @@
 package com.idiomcentric.service
 
+import io.micronaut.cache.annotation.CacheConfig
+import io.micronaut.cache.annotation.Cacheable
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.http.uri.UriBuilder
 import io.micronaut.retry.annotation.Retryable
 import jakarta.inject.Singleton
-import kotlinx.coroutines.FlowPreview
 import java.net.URI
 
 @Singleton
+@CacheConfig("headlines")
 open class RedditLowLevelClient(
     @param:Client(RedditConfiguration.REDDIT_API_URL)
     private val httpClient: HttpClient,
@@ -22,11 +24,10 @@ open class RedditLowLevelClient(
         .queryParam("t", configuration.t)
         .build()
 
-    @OptIn(FlowPreview::class)
     @Retryable(attempts = "3", delay = "1")
+    @Cacheable
     open fun fetchTopPosts(): List<RedditChildData> {
         val request = HttpRequest.GET<RedditResponse>(uri)
-
         return httpClient.toBlocking()
             .retrieve(request, RedditResponse::class.java).data.children.map { it.data }
 
