36928f9fc30921a2be37abd0bc53b91d580f3027
conorfennell <fennell.conor@gmail.com>
2022-01-01 22:11:55 +0000

    require the configuration

--- src/main/kotlin/com/idiomcentric/service/reddit/RedditConfiguration.kt
+++ src/main/kotlin/com/idiomcentric/service/reddit/RedditConfiguration.kt
@@ -1,14 +1,28 @@
 package com.idiomcentric.service.reddit
 
 import io.micronaut.context.annotation.ConfigurationProperties
+import javax.validation.constraints.NotNull
 
 @ConfigurationProperties("reddit")
 open class RedditConfiguration {
+    @NotNull
     var host: String? = null
+
+    @NotNull
     var limit: Int? = null
+
+    @NotNull
     var port: Int? = null
+
+    @NotNull
     var query: String? = null
+
+    @NotNull
     var rPath: String? = null
+
+    @NotNull
     var subReddit: String? = null
+
+    @NotNull
     var t: String? = null
 }
