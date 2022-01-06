7675bceb3f72e4a53285d725691f6aed2cf66ab3
conorfennell <fennell.conor@gmail.com>
2022-01-06 20:31:56 +0000

    add reddit call without saving it

---
+++ src/main/kotlin/com/idiomcentric/scheduler/RedditJob.kt
@@ -0,0 +1,19 @@
+package com.idiomcentric.scheduler
+
+import com.idiomcentric.service.reddit.RedditClient
+import io.micronaut.scheduling.annotation.Scheduled
+import jakarta.inject.Singleton
+import mu.KLogger
+import mu.KotlinLogging
+
+val logger: KLogger = KotlinLogging.logger {}
+
+@Singleton
+open class RedditJob(private val redditClient: RedditClient) {
+
+    @Scheduled(fixedDelay = "\${jobs.reddit.fixedDelay}")
+    fun execute() {
+        val posts = redditClient.fetchTopPosts()
+        logger.info(posts.toString())
+    }
+}
