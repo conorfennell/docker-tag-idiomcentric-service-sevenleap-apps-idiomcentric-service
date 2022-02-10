44abe93e9689d76204795f11f5a97b337f951e0e
conorfennell <fennell.conor@gmail.com>
2022-02-10 08:00:03 +0000

    add foreign key to chunk encode

--- src/main/kotlin/com/idiomcentric/dao/chunkencode/ChunkEncodeTable.kt
+++ src/main/kotlin/com/idiomcentric/dao/chunkencode/ChunkEncodeTable.kt
@@ -1,5 +1,6 @@
 package com.idiomcentric.dao.chunkencode
 
+import com.idiomcentric.dao.chunk.ChunkTable
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.timestamp
 import java.time.Instant
@@ -7,6 +8,7 @@ import java.util.UUID
 
 object ChunkEncodeTable : Table("CHUNK_ENCODE") {
     val id = uuid("ID")
+    val chunkId = reference("CHUNK_ID", ChunkTable.id).uniqueIndex()
     val question = text("QUESTION")
     var answer = text("TEXT") // TODO what should collate be set to?
     val createdAt = timestamp("CREATED_AT")
