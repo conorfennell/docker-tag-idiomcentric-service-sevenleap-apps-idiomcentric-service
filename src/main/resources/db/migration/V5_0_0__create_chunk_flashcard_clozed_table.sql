b3e934f0521093b61814f67df71bbe71162a7a66
conorfennell <fennell.conor@gmail.com>
2022-02-23 09:15:23 +0000

    make chunk_id not null in clozed flashcard

--- src/main/resources/db/migration/V5_0_0__create_chunk_flashcard_clozed_table.sql
+++ src/main/resources/db/migration/V5_0_0__create_chunk_flashcard_clozed_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS CLOZED_FLASHCARD (
     id uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-    chunk_id uuid,
+    chunk_id uuid NOT NULL,
     sentence TEXT NOT NULL,
     clozed_positions TEXT NOT NULL,
     updated_at TIMESTAMP NOT NULL,
